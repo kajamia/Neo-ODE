6092206d2bed7ee75627280f1e3a5f45f7a8dd23
ginamia13 <kaja.misurec@chello.at>
2023-11-30 15:12:13 +0100

    aligned function names

--- src/Newton.h
+++ src/Newton.h
@@ -2,6 +2,7 @@
 #define Newton_h
 
 #include "nonlinfunc.h"
+#include "matrix.h"
 
 namespace ASC_ode
 {
@@ -18,7 +19,7 @@ namespace ASC_ode
         func->Evaluate(x, res);
         // cout << "|res| = " << L2Norm(res) << endl;
         func->EvaluateDeriv(x, fprime);
-        CalcInverse(fprime);
+        Inverse(fprime);
         x -= fprime*res;
 
         double err= L2Norm(res);
